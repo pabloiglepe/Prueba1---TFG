94c787400c271d695d0f894d2733c652235944c7
pabloiglepe <pabloiglep2@gmail.com>
2026-03-23 21:31:53 +0100

    Sistema de acceso a la aplicación

--- src/resources/views/layouts/guest.blade.php
+++ src/resources/views/layouts/guest.blade.php
@@ -23,14 +23,12 @@
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
-            <a href="/" wire:navigate>
+            <!-- <a href="/" wire:navigate> -->
                 <!-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> -->
-
                 <img src="{{ asset('images/padel-logo.png') }}"
                     alt="Logo Padel"
                     class="w-25 h-20 md:w-40 md:h-40 object-contain drop-shadow-md">
-
-            </a>
+            <!-- </a> -->
         </div>
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
